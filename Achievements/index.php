281ce824421250596bdcc114c0acba54b6246bf2
kstuckman <k8thesupergr8@yahoo.com>
2013-10-16 14:27:47 -0700

    Initial Commit

---
+++ Achievements/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>CS90SI Project 2</title>
+	<script type="text/javascript" src="validation.js"></script>
+</head>
+<body>
+
+<?php for ($i = 0; $i < 20; $i++) 
+    {?>
+    <img src="gold-medal.png" alt="my image" />
+<?php
+    }?>
+
+</body>
+</html>
